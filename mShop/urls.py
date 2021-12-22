3a9e1265e4c604918a536907be50cf00f9180207
Hoepless <kanybekov.abay.reserve@gmail.com>
2021-12-22 19:25:22 +0600

    docker fixed

--- mShop/urls.py
+++ mShop/urls.py
@@ -1,19 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework_swagger.views import get_swagger_view
-
-
-schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('main/', include('main.urls')),
-    re_path(r'^swagger/$', schema_view)
 ]
 
 if settings.DEBUG:
